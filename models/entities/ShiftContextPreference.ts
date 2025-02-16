6917339d5c81f7ad9c3b66dd9c6bea4ce160fab6
Luke Hamann <135341515+luke-hamann@users.noreply.github.com>
2025-02-15 19:24:52 -0600

    Fix shift context preference model

--- models/entities/ShiftContextPreference.ts
+++ models/entities/ShiftContextPreference.ts
@@ -6,16 +6,19 @@ export default class ShiftContextPreference {
   public teamMember: TeamMember | null = null;
   public shiftContextId: number = 0;
   public shiftContext: ShiftContext | null = null;
+  public isPreferable: boolean = false;
 
   public constructor(
     teamMemberId: number,
     teamMember: TeamMember | null,
     shiftContextId: number,
     shiftContext: ShiftContext | null,
+    isPreferable: boolean
   ) {
     this.teamMemberId = teamMemberId;
     this.teamMember = teamMember;
     this.shiftContextId = shiftContextId;
     this.shiftContext = shiftContext;
+    this.isPreferable = isPreferable;
   }
 }
