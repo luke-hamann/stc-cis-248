eda10efbbf5eea9aaea9712a07466b455278615d
Luke Hamann <135341515+luke-hamann@users.noreply.github.com>
2025-02-20 12:49:37 -0600

    Add cookie parsing

--- models/controllerLayer/Context.ts
+++ models/controllerLayer/Context.ts
@@ -5,6 +5,7 @@ import ResponseWrapper from "./ResponseWrapper.ts";
  */
 export default class Context {
   public request: Request;
+  public requestCookies: Map<string, string>;
   public response: ResponseWrapper;
   public match: string[] = [];
   public csrf_token: string = "";
@@ -17,5 +18,19 @@ export default class Context {
   public constructor(request: Request, response: ResponseWrapper) {
     this.request = request;
     this.response = response;
+
+    this.requestCookies = new Map<string, string>();
+    const cookieHeader = request.headers.get("Cookie") ?? "";
+    const cookiePairs = cookieHeader
+      .split(";")
+      .map((value) => value.trim())
+      .filter((value) => value.length > 0)
+      .map((pair) => [...pair.split("="), "", ""].slice(0, 2));
+
+    for (let [key, value] of cookiePairs) {
+      key = decodeURIComponent(key);
+      value = decodeURIComponent(value);
+      this.requestCookies.set(key, value);
+    }
   }
 }
