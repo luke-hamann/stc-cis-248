9a97b42f31c88f34cefc4b1dfd8a67102623a923
Luke Hamann <135341515+luke-hamann@users.noreply.github.com>
2025-03-17 07:08:14 -0500

    Test and document date library

--- src/_dates/DateLib.ts
+++ src/_dates/DateLib.ts
@@ -1,18 +1,38 @@
+/** A static class for performing date calculations */
 export default class DateLib {
+  /**
+   * Adds a number of days to a date and returns the new date
+   * @param date The original date
+   * @param days A number of days to add
+   * @returns The new date
+   */
   public static addDays(date: Date, days: number): Date {
-    const newDate = new Date(date.getTime());
+    const newDate = new Date(date);
     newDate.setDate(newDate.getDate() + days);
     return newDate;
   }
 
+  /**
+   * Rolls back a date to the most recent Sunday and returns the new date
+   * 
+   * The date does not change if it is a Sunday
+   * @param date The date
+   * @returns The new date
+   */
   public static floorToSunday(date: Date): Date {
     return this.addDays(date, -date.getDay());
   }
 
+  /**
+   * Gets a list of dates within a date range, inclusive
+   * @param start The start date
+   * @param end The end date
+   * @returns The list of dates within range
+   */
   public static getDatesInRange(start: Date, end: Date): Date[] {
     const dates = [];
 
-    let date = new Date(start.getTime());
+    let date = new Date(start);
     while (date.getTime() <= end.getTime()) {
       dates.push(date);
       date = this.addDays(date, 1);
@@ -21,24 +41,15 @@ export default class DateLib {
     return dates;
   }
 
+  /**
+   * Calculates the difference between 2 dates in days
+   * @param date1 The first date
+   * @param date2 The second date
+   * @returns The difference in days
+   */
   public static differenceInDays(date1: Date, date2: Date): number {
     return Math.round(
       (date2.getTime() - date1.getTime()) / (24 * 60 * 60 * 1000),
     );
-  }
-
-  public static getAge(birthDate: Date, today: Date): number {
-    birthDate.setHours(0, 0, 0, 0);
-    today.setHours(0, 0, 0, 0);
-
-    let count = 0;
-
-    while (true) {
-      birthDate.setFullYear(birthDate.getFullYear() + 1);
-      if (birthDate.getTime() > today.getTime()) break;
-      count++;
-    }
-
-    return count;
   }
 }
--- tests/DateLib.test.ts
+++ tests/DateLib.test.ts
@@ -2,52 +2,40 @@ import { assertEquals } from "https://deno.land/std@0.104.0/testing/asserts.ts";
 import DateLib from "../src/_dates/DateLib.ts";
 
 Deno.test("date lib add days positive", () => {
-  const date = new Date("1999-12-31T00:00:00.000Z");
+  const date = new Date(1999, 11, 31);
 
   const newDate = DateLib.addDays(date, 3);
 
-  assertEquals(newDate.toISOString(), "2000-01-03T00:00:00.000Z");
+  assertEquals(newDate.getFullYear(), 2000);
+  assertEquals(newDate.getMonth(), 0),
+  assertEquals(newDate.getDate(), 3)
 });
 
 Deno.test("date lib add days negative", () => {
-  const date = new Date("1999-12-31T00:00:00.000Z");
+  const date = new Date(1999, 11, 31);
 
   const newDate = DateLib.addDays(date, -5);
 
-  assertEquals(newDate.toISOString(), "1999-12-26T00:00:00.000Z");
+  assertEquals(newDate.getFullYear(), 1999);
+  assertEquals(newDate.getMonth(), 11);
+  assertEquals(newDate.getDate(), 26)
 });
 
 Deno.test("date lib floor days", () => {
-  const date = new Date("2025-03-14T00:00:00.000Z");
+  const date = new Date(2025, 2, 14);
 
   const newDate = DateLib.floorToSunday(date);
 
-  assertEquals(newDate.toISOString(), "2025-03-09T00:00:00.000Z");
+  assertEquals(newDate.getFullYear(), 2025);
+  assertEquals(newDate.getMonth(), 2);
+  assertEquals(newDate.getDate(), 9)
 });
 
 Deno.test("date lib get dates in range", () => {
-  const start = new Date("2025-03-13T00:00:00.000Z");
-  const end = new Date("2025-04-02T00:00:00.000Z");
+  const start = new Date("2025-03-13");
+  const end = new Date("2025-04-02");
 
   const dates = DateLib.getDatesInRange(start, end);
 
   assertEquals(dates.length, 21);
-});
-
-Deno.test("date lib get age", () => {
-  const start = new Date("2025-01-03");
-  const end = new Date("2039-01-02");
-
-  const age = DateLib.getAge(start, end);
-
-  assertEquals(age, 13);
-});
-
-Deno.test("date lib get age", () => {
-  const start = new Date("2025-01-03");
-  const end = new Date("2039-01-03");
-
-  const age = DateLib.getAge(start, end);
-
-  assertEquals(age, 14);
 });
