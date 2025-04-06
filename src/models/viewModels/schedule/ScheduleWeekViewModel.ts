f08af9612fde04d608d8639d7edd53f53c6c7ff7
Luke Hamann <135341515+luke-hamann@users.noreply.github.com>
2025-04-06 11:50:13 -0500

    Add doc comments

--- src/models/viewModels/schedule/ScheduleWeekViewModel.ts
+++ src/models/viewModels/schedule/ScheduleWeekViewModel.ts
@@ -4,11 +4,22 @@ import Schedule from "../../entities/Schedule.ts";
 import ScheduleWarnings from "../../entities/ScheduleWarnings.ts";
 import ViewModel from "../_shared/_ViewModel.ts";
 
+/** The view model for the schedule week editor page */
 export default class ScheduleWeekViewModel extends ViewModel {
+  /** The first day of the schedule week being viewed */
   public currentWeek: Date;
+
+  /** The schedule table */
   public schedule: Schedule | null;
+
+  /** The schedule warnings */
   public warnings: ScheduleWarnings;
 
+  /** Constructs the view model
+   * @param currentWeek 
+   * @param schedule 
+   * @param warnings 
+   */
   constructor(
     currentWeek: Date,
     schedule: Schedule | null,
@@ -20,22 +31,29 @@ export default class ScheduleWeekViewModel extends ViewModel {
     this.warnings = warnings;
   }
 
+  /** Formats a date as a string in yyyy/mm/dd format
+   * @param date The date
+   * @returns The string
+   */
   private formatDatePath(date: Date): string {
-    return BetterDate.fromDate(date).toDateString().replaceAll("-", "/");
+    return BetterDate.fromDate(date).toDateString("/");
   }
 
+  /** Generates a url to the form for clearing the current schedule week */
   public get clearLink(): string {
     const start = this.formatDatePath(this.currentWeek);
     const end = this.formatDatePath(DateLib.addDays(this.currentWeek, 6));
     return `/schedule/clear/${start}/through/${end}/`;
   }
 
+  /** Generates a url to the form for copying the current schedule week k */
   public get copyLink(): string {
     const start = this.formatDatePath(this.currentWeek);
     const end = this.formatDatePath(DateLib.addDays(this.currentWeek, 6));
     return `/schedule/copy/${start}/through/${end}/`;
   }
 
+  /** Generates url to the form for exporting the current schedule week */
   public get exportLink(): string {
     const start = this.formatDatePath(this.currentWeek);
     const end = this.formatDatePath(DateLib.addDays(this.currentWeek, 6));
@@ -80,8 +98,7 @@ export default class ScheduleWeekViewModel extends ViewModel {
     return BetterDate.fromDate(previousWeek).toDateString("/");
   }
 
-  /** Get the string representation of the date one week after the current week
-   */
+  /** Get the string representation of the date one week after the current week */
   public get nextWeekString(): string {
     const nextWeek = new Date(this.currentWeek.getTime());
     nextWeek.setDate(nextWeek.getDate() + 7);
