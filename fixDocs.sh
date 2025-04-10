d1c0cf15d85f11f3533bb7b1c32c09cae2cda670
Luke Hamann <135341515+luke-hamann@users.noreply.github.com>
2025-04-10 14:13:46 -0500

    Test fix docs

--- fixDocs.sh
+++ fixDocs.sh
@@ -1,3 +1,3 @@
 cd ./docs
-find . -type f -name '*.html' -exec sed -i -e 's/href="file:&#x2F;&#x2F;&#x2F;home&#x2F;runner&#x2F;work&#x2F;stc-cis-248&#x2F;stc-cis-248&#x2F;/href="https:&#x2F;&#x2F;github\.com&#x2F;luke-hamann&#x2F;stc-cis-248&#x2F;blob&#x2F;master&#x2F;/g' {} \;
+find . -type f -name '*.html'
 cd ..
