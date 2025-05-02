7f927092eccd1c0ad1aeb6617b725950d80e7f45
Luke Hamann <135341515+luke-hamann@users.noreply.github.com>
2025-05-02 18:09:52 -0500

    Patch Database class

--- src/models/repositories/_Database.ts
+++ src/models/repositories/_Database.ts
@@ -3,15 +3,15 @@ import mysql from "npm:mysql2/promise";
 
 /** Represents a database connection */
 export default class Database {
-  /** The database client connection pool 
-   * 
+  /** The database client connection pool
+   *
    * Connects to a MySQL database based on environment variables:
    *
    * * DATABASE_HOSTNAME
    * * DATABASE_USERNAME
    * * DATABASE_NAME
    * * DATABASE_PASSWORD
-  */
+   */
   private _pool = mysql.createPool({
     host: Deno.env.get("DATABASE_HOSTNAME"),
     user: Deno.env.get("DATABASE_USERNAME"),
@@ -22,13 +22,13 @@ export default class Database {
   /** Executes a SQL query against a MySQL database using given parameters
    * @param sql The SQL query
    * @param params An array of positional parameters for the query
-   * @returns A promise of the query {@link ExecuteResult}
+   * @returns An object containing a rows property with the result rows
    */
   public async execute(
     sql: string,
     params?: (undefined | null | string | number | boolean | Date)[],
   ): Promise<{ rows?: any[] }> {
-    const rows = (await this._pool.query(sql, params))[0] as any[];
+    const rows = (await this._pool.execute(sql, params))[0] as any[];
     return { rows };
   }
 }
